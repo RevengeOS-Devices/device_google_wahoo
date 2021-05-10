05fe1a38a9625d5513cb0cec656fbe42bd4dba7e
MadScientistA <anmolzagrawal@gmail.com>
2021-05-10 19:56:25 +0200

    wahoo: Set OPA property to enable google assistant
    (cherry picked from commit 796bfaa87cc59ecab44f0bc5c8c54f9547b87588)

--- device.mk
+++ device.mk
@@ -740,6 +740,10 @@ include hardware/google/pixel/thermal/device.mk
 # power HAL
 -include hardware/google/pixel/power-libperfmgr/hidl/device.mk
 
+# Enable missing props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device=true
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
