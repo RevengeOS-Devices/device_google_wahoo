1b5bed97fa92523ddb4aa7bb7abe9c0cbe1e5ef7
MadScientistA <anmolzagrawal@gmail.com>
2021-05-10 20:09:26 +0200

    wahoo: Adding VENDOR_SECURITY_PATCH level definition
    (cherry picked from commit 0658aaaa3b2bbbc68fbd93d92436a32ba50eb6f0)

--- device.mk
+++ device.mk
@@ -774,6 +774,9 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
+# Setting vendor SPL
+VENDOR_SECURITY_PATCH := "2020-10-05"
+
 # Shared java libs
 PRODUCT_PACKAGES += \
     com.android.nfc_extras
